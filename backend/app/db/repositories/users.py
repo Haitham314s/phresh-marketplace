8754db2dd496f371144d1450e8a7b7748324fbe7
Haitham314s <haitham314s@gmail.com>
2023-10-10 23:43:27 +0400

    get user by email and username class methods

--- backend/app/db/repositories/users.py
+++ backend/app/db/repositories/users.py
@@ -1,11 +1,27 @@
+from fastapi import HTTPException, status
+
 from app.models.schemas.user import UserCreateIn, UserPublicOut
 from app.models.user import User
 
 
 class UserRepository:
+    async def get_user_by_email(self, email: str) -> UserPublicOut | None:
+        user = await User.get_or_none(email=email)
+        return UserPublicOut.model_validate(user) if user is not None else None
+
+    async def get_user_by_username(self, username: str) -> UserPublicOut | None:
+        user = await User.get_or_none(username=username)
+        return UserPublicOut.model_validate(user) if user is not None else None
+
     async def register_new_user(self, new_user: UserCreateIn) -> UserPublicOut:
+        if await self.get_user_by_email(new_user.email) is not None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Email already taken"
+            )
+        if await self.get_user_by_username(new_user.username) is not None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Username already taken"
+            )
+
         user = await User.create(**new_user.model_dump())
         return UserPublicOut.model_validate(user)
-
-    async def get_user_by_email(self, email: str) -> UserPublicOut or None:
-        return await User.get_or_none(email=email)
